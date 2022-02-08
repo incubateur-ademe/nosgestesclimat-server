0e684e1e3ac5948764c6a853e15be73811c2a448
Mael <mael.thomas@data.gouv.fr>
2022-02-08 11:10:26 +0100

    Ajout de l'URL de base de prod

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-mongoose.Promise = require("bluebird");
-const url = "mongodb://localhost:27017/survey";
-const connect = mongoose.connect(url, { useNewUrlParser: true });
-module.exports = connect;
+const mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
+const url = process.env.SCALINGO_MONGO_URL || 'mongodb://localhost:27017/survey'
+const connect = mongoose.connect(url, { useNewUrlParser: true })
+module.exports = connect
