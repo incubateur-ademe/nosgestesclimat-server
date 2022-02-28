6602844cd9543466b6094e7724a3964a73d6a025
Mael <mael.thomas@data.gouv.fr>
2022-02-28 15:47:47 +0100

    Téléchargement des réponses en CSV

--- answersRoute.js
+++ answersRoute.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser')
 const connectdb = require('./database')
 const Answers = require('./AnswerSchema')
 
+const { Parser } = require('json2csv')
+const fields = ['byCategory', 'total', 'progress']
+const parser = new Parser({ fields })
+
 const router = express.Router()
 
 router.route('/:room').get((req, res, next) => {
@@ -10,19 +14,28 @@ router.route('/:room').get((req, res, next) => {
     throw new Error('Unauthorized. A valid survey name must be provided')
   }
 
+  const csv = req.query.format === 'csv'
+
   connectdb.then((db) => {
     let data = Answers.find({ survey: req.params.room })
     data.then((answers) => {
-      res.setHeader('Content-Type', 'application/json')
-      res.statusCode = 200
-      res.json(
-        answers.map(({ byCategory, data, progress, id }) => ({
-          byCategory,
-          data,
-          progress,
-          id,
-        }))
-      )
+      if (!csv) {
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200
+        res.json(answers.map(({ data, id }) => ({ data, id })))
+      } else {
+        try {
+          const json = answers.map((answer) =>
+            Object.fromEntries(
+              fields.map((field) => [field, answer.data[field]])
+            )
+          )
+          const csv = parser.parse(json)
+          res.attachment(`sondage-NGC-${req.params.room}.csv`).send(csv)
+        } catch (err) {
+          console.error('Error parsing JSON survey answers as CSV', err)
+        }
+      }
     })
   })
 })
