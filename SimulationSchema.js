af29d6a3c339f56e902cc9916c0bea4e439ac53b
Mael <laem@kont.me>
2023-03-23 16:16:21 +0100

    Commentaire sur le schéma de simu

--- SimulationSchema.js
+++ SimulationSchema.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 const SimulationSchema = new Schema(
   {
     id: String,
-    data: Object, //TODO temporary format just to try a v0
+    data: Object, //TODO temporary format just to try a v0. Should be specified in a v2 ? Or let loose ?
   },
   {
     timestamps: true,
