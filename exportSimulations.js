c573d078a0444440971f4af1cef642088e623015
Mael <laem@kont.me>
2023-04-03 18:37:08 +0200

    Date dans le fichier d'export

--- exportSimulations.js
+++ exportSimulations.js
@@ -5,7 +5,12 @@ const fs = require('fs')
 connectdb.then((db) => {
   let request = Simulation.find()
   request.then((simulations) => {
-    fs.writeFileSync('./export/simulations.json', JSON.stringify(simulations))
+    const date = new Date().toLocaleDateString('fr-FR').replace(/\//g, '-')
+    fs.writeFileSync(
+      `./export/simulations-${date}.json`,
+      JSON.stringify(simulations)
+    )
+    //fs.writeFileSync('./export/simulations.csv', JSON.stringify(simulations))
     db.disconnect()
     return console.log('Fichier écrit')
   })
