36d1fa3ca34230683831d19ed0e99927ef4245ca
Mael <laem@kont.me>
2023-04-03 18:46:44 +0200

    WIP script CSV

--- exportSimulations.js
+++ exportSimulations.js
@@ -10,8 +10,46 @@ connectdb.then((db) => {
       `./export/simulations-${date}.json`,
       JSON.stringify(simulations)
     )
-    //fs.writeFileSync('./export/simulations.csv', JSON.stringify(simulations))
+    toCSV(simulations).then((content) =>
+      fs.writeFileSync('./export/simulations.csv', JSON.stringify(content))
+    )
     db.disconnect()
     return console.log('Fichier écrit')
   })
 })
+
+const url = 'https://data.nosgestesclimat.fr/co2-model.FR-lang.fr.json'
+const categories = [
+  'logement',
+  'transport',
+  'alimentation',
+  'divers',
+  'services sociétaux',
+]
+
+const toCSV = async (list) => {
+  const response = await fetch(url)
+  if (!response.ok) console.log('Oups')
+  const rules = await response.json()
+
+  const questionRules = Object.entries(rules)
+    .map(([dottedName, v]) => ({ ...v, dottedName }))
+    .filter((el) => el && el.question)
+  const questionDottedNames = questionRules.map((rule) => rule.dottedName)
+
+  // We need to expose the full list of questions of the model in order to index the CSV
+  // Then fill with value | 'default' | ''
+  const header = ['userID', ...categories, 'total', ...questionDottedNames]
+  const questionValue = (data, question) => {
+    const value = data.situation[question]
+    if (typeof value === 'object') return value.valeur
+    else return value
+  }
+  const newList = list.map((data) => [
+    data.id,
+    ...categories.map((category) => data.results.categories[category]),
+    data.results.total,
+    ...questionDottedNames.map((question) => questionValue(data, question)),
+  ])
+  return [header, ...newList].join('\n')
+}
