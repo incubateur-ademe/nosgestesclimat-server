982cb3c11f78d2fc15aa1fca74aa6c0a08522997
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-10-03 11:07:36 +0200

    feat: Ajoute Origin

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const origin =
       ]
     : [
         'https://nosgestesclimat.fr',
+        'https://sondages.nosgestesclimat.fr',
         'https://nosgestesclimat.vercel.app',
         'https://nosgestesclimat-git-preprod-nos-gestes-climat.vercel.app'
       ]
