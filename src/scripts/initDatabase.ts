990a8588977ea7430cac3d06629b2df1cd499f4d
Benjamin Arias <bjlaa@proton.me>
2024-01-26 15:21:21 +0100

    Update initDatabase.ts

--- src/scripts/initDatabase.ts
+++ src/scripts/initDatabase.ts
@@ -6,4 +6,6 @@ mongoose.Promise = bluebird
 const url =
   process.env.SCALINGO_MONGO_URL || 'mongodb://127.0.0.1:27017/nosgestesclimat'
 
+mongoose.set('strictQuery', false)
+
 export default mongoose.connect(url)
