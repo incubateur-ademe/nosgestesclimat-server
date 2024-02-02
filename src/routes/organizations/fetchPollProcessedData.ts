3be319e0ef9e6c0f4cf6916deeb1833bc4b13628
Benjamin Arias <bjlaa@proton.me>
2024-02-02 15:05:38 +0100

    feat: Ajoute la route de fetch de données de sondage

--- src/routes/organizations/fetchPollProcessedData.ts
+++ src/routes/organizations/fetchPollProcessedData.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express'
 import { Organization } from '../../schemas/OrganizationSchema'
 import { setSuccessfulJSONResponse } from '../../utils/setSuccessfulResponse'
 import { authentificationMiddleware } from '../../middlewares/authentificationMiddleware'
+import { processPollData } from '../../helpers/organizations/processPollData'
+import { Simulation } from '../../schemas/SimulationSchema'
 
 const router = express.Router()
 
@@ -10,19 +12,39 @@ router
   .use(authentificationMiddleware)
   .post('/', async (req: Request, res: Response) => {
     const email = req.body.email
+    const fileName = req.body.fileName
 
     if (!email) {
       return res.status(403).json('No owner email provided.')
     }
+
+    if (!fileName) {
+      return res.status(403).json('No fileName provided.')
+    }
+
+    // The model version to fetch must be defined by the front-end
+    const rules = await import(
+      `@incubateur-ademe/nosgestesclimat/public/${fileName}`
+    ).then((module) => module.default)
 
     try {
       const organizationFound = await Organization.findOne({
         'administrators.email': email,
       }).populate('polls.simulations')
 
+      if (!organizationFound) {
+        return res.status(403).json('No organization found.')
+      }
+
+      const pollData = processPollData({
+        simulations: organizationFound?.polls[0]
+          ?.simulations as unknown as Simulation[],
+        rules,
+      })
+
       setSuccessfulJSONResponse(res)
 
-      res.json(organizationFound)
+      res.json(pollData)
     } catch (error) {
       res.status(403).json('No organization found.')
     }
