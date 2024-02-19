4db982b85752ab27257c171f87c8d14e594cb173
Benjamin Arias <bjlaa@proton.me>
2024-02-19 16:00:56 +0100

    feat: Ajoute une origin acceptée

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ const origin =
         'https://preprod.nosgestesclimat.fr',
         'https://nosgestesclimat.vercel.app',
         'https://nosgestesclimat-git-preprod-nos-gestes-climat.vercel.app',
-        'https://nosgestesclimat-git-parcours-orga-inscription-nos-gestes-climat.vercel.app',
+        'https://nosgestesclimat-git-parcours-orga-dashboard-ademe.vercel.app',
       ]
 
 app.use(
