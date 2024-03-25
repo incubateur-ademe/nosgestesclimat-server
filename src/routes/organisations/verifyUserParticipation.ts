1e3c4d944f9ea0fc7e4ddfd3d94bddd54ca0ac88
Benjamin Arias <bjlaa@proton.me>
2024-03-25 14:30:30 +0100

    feat: Ajoute le check sur l'email

--- src/routes/organisations/verifyUserParticipation.ts
+++ src/routes/organisations/verifyUserParticipation.ts
@@ -9,49 +9,60 @@ const router = express.Router()
 /**
  * Verify if a user has already participated in a poll
  */
-router
-  .route('verify-user-participation')
-  .post(async (req: Request, res: Response) => {
-    const pollSlug = req.body.pollSlug
-    const userId = req.body.userId
+router.route('/').post(async (req: Request, res: Response) => {
+  const pollSlug = req.body.pollSlug
+  const userId = req.body.userId
+  const email = req.body.email
 
-    if (!pollSlug) {
-      return res.status(500).send('You must provide a poll slug')
+  if (!pollSlug) {
+    return res.status(500).send('You must provide a poll slug')
+  }
+
+  if (!userId) {
+    return res.status(500).send('You must provide a user id')
+  }
+
+  try {
+    const poll = await findPopulatedPollBySlug(pollSlug)
+
+    if (!poll) {
+      return res.status(404).send('This poll does not exist')
     }
 
-    if (!userId) {
-      return res.status(500).send('You must provide a user id')
-    }
-
-    try {
-      const poll = await findPopulatedPollBySlug(pollSlug)
-
-      if (!poll) {
-        return res.status(404).send('This poll does not exist')
+    const hasUserAlreadyParticipated = (
+      poll.simulations as unknown as SimulationType[]
+    ).some((simulation) => {
+      if ((simulation?.user as any).userId === userId) {
+        return true
       }
 
-      const hasUserAlreadyParticipated = (
-        poll.simulations as unknown as SimulationType[]
-      ).some((simulation) => (simulation?.user as any).userId)
-
-      let organisation = undefined
-      if (hasUserAlreadyParticipated) {
-        organisation = await Organisation.findOne({
-          polls: {
-            $in: poll._id,
-          },
-        })
+      if (
+        (simulation?.user as any).email &&
+        (simulation?.user as any).email === email
+      ) {
+        return true
       }
+    })
 
-      setSuccessfulJSONResponse(res)
+    let organisation = undefined
 
-      res.json({
-        hasUserAlreadyParticipated,
-        organisationSlug: organisation?.slug,
+    if (hasUserAlreadyParticipated) {
+      organisation = await Organisation.findOne({
+        polls: {
+          $in: poll._id,
+        },
       })
-    } catch (error) {
-      return res.status(500).send('Error while fetching poll')
     }
-  })
+
+    setSuccessfulJSONResponse(res)
+
+    res.json({
+      hasUserAlreadyParticipated,
+      organisationSlug: organisation?.slug,
+    })
+  } catch (error) {
+    return res.status(500).send('Error while fetching poll')
+  }
+})
 
 export default router
