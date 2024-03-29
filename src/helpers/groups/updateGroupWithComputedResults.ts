169ef20b4bc72f99cd90718e000657e5464ede18
Benjamin Arias <bjlaa@proton.me>
2024-03-29 11:27:11 +0100

    fix: Corrige le helper de mise à jour des computedResults

--- src/helpers/groups/updateGroupWithComputedResults.ts
+++ src/helpers/groups/updateGroupWithComputedResults.ts
@@ -1,27 +1,36 @@
-import { Document } from "mongoose";
-import { GroupType } from "../../schemas/GroupSchema";
-import { Simulation, SimulationType } from "../../schemas/SimulationSchema";
-import { computeResults } from "../simulation/computeResults";
+import { Document } from 'mongoose'
+import { GroupType } from '../../schemas/GroupSchema'
+import { Simulation, SimulationType } from '../../schemas/SimulationSchema'
+import { computeResults } from '../simulation/computeResults'
 
 export async function updateGroupWithComputedResults(group: GroupType) {
-  if (group.participants.every((participant) => (participant.simulation as unknown as SimulationType)?.computedResults)) {
+  // Do not update the simulations if they already have computed results
+  if (
+    !group.participants ||
+    group.participants.length === 0 ||
+    group.participants.every(
+      (participant) =>
+        !!(participant.simulation as unknown as SimulationType)?.computedResults
+    )
+  ) {
     return
   }
 
-  if (group.participants) {
-    for (const participant of group.participants) {
-      if (participant.simulation) {
-        // We can add computed results here
-        const simulationFound = await Simulation.findById(participant.simulation)
+  for (const participant of group.participants) {
+    // Should be populated
+    if (participant.simulation) {
+      // We can add computed results here
+      const simulationFound = await Simulation.findById(
+        (participant.simulation as unknown as SimulationType)._id
+      )
 
-        if (!simulationFound || simulationFound.computedResults) {
-          continue
-        }
-
-        simulationFound.computedResults = computeResults(simulationFound)
-        await simulationFound.save()
-
+      if (!simulationFound || simulationFound.computedResults) {
+        continue
       }
+
+      simulationFound.computedResults = computeResults(simulationFound)
+
+      await simulationFound.save()
     }
   }
 }
