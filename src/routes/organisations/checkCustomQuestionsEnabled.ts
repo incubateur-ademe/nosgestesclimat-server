5ce1e85eee75337b8a52fdb4601a20f060a56792
Benjamin Arias <bjlaa@proton.me>
2024-05-07 15:51:48 +0200

    feat: Use get instead of post

--- src/routes/organisations/checkCustomQuestionsEnabled.ts
+++ src/routes/organisations/checkCustomQuestionsEnabled.ts
@@ -5,38 +5,42 @@ import { config } from '../../config'
 
 const router = express.Router()
 
-router.route('/').post(async (req: Request, res: Response) => {
-  try {
-    const slug = req.body.slug
+router
+  .route('/:slug?')
+  .get(async (req: Request & { params: { slug: string } }, res: Response) => {
+    try {
+      const slug = req.params.slug
 
-    if (!slug) {
-      return res.status(403).json('Error. A slug must be provided.')
+      console.log(req.params)
+
+      if (!slug) {
+        return res.status(403).json('Error. A slug must be provided.')
+      }
+
+      const organisationFound = await Organisation.findOne({
+        slug,
+      })
+
+      if (!organisationFound) {
+        return res.status(403).json('Error. Organisation not found.')
+      }
+
+      setSuccessfulJSONResponse(res)
+
+      // Verify if the organisation is authorised to use custom questions
+      if (
+        config.organisationIdsWithCustomQuestionsEnabled?.includes(
+          organisationFound._id.toString()
+        )
+      ) {
+        res.json(true)
+      } else {
+        res.json(false)
+      }
+    } catch (error) {
+      console.log(error)
+      return res.status(403).json(error)
     }
-
-    const organisationFound = await Organisation.findOne({
-      slug,
-    })
-
-    if (!organisationFound) {
-      return res.status(403).json('Error. Organisation not found.')
-    }
-
-    setSuccessfulJSONResponse(res)
-
-    // Verify if the organisation is authorised to use custom questions
-    if (
-      config.organisationIdsWithCustomQuestionsEnabled?.includes(
-        organisationFound._id.toString()
-      )
-    ) {
-      res.json(true)
-    } else {
-      res.json(false)
-    }
-  } catch (error) {
-    console.log(error)
-    return res.status(403).json(error)
-  }
-})
+  })
 
 export default router
