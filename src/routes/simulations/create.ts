fdd4d87c2f5b9a1eaf6cd6ea7fa0ee4bed715080
Benjamin Arias <bjlaa@proton.me>
2024-05-07 17:45:15 +0200

    Update create.ts

--- src/routes/simulations/create.ts
+++ src/routes/simulations/create.ts
@@ -41,7 +41,6 @@ router.route('/').post(async (req, res) => {
 
   // If there is no user found or created, we return an error
   if (!userDocument) {
-    console.log('NO USER FOUND')
     return res
       .status(500)
       .send('Error while creating or searching for the user.')
@@ -128,7 +127,6 @@ router.route('/').post(async (req, res) => {
 
     console.log(`Simulation created: ${simulationSaved._id}`)
   } catch (error) {
-    console.error(error)
     return res.status(401).send('Error while creating simulation.')
   }
 })
