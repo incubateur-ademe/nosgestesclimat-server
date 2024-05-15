8b350db500788d7f661740f2767b75431069cd7a
Benjamin Arias <bjlaa@proton.me>
2024-05-15 17:21:38 +0200

    Update unformatSituation.ts

--- src/utils/unformatSituation.ts
+++ src/utils/unformatSituation.ts
@@ -1,6 +1,12 @@
 export function unformatSituation(situation?: { [key: string]: any }) {
   return Object.entries({ ...situation } as { [key: string]: any }).reduce(
     (acc: { [key: string]: any }, [key, value]: [string, any]) => {
+      // Key is not formatted
+      if (!key.includes('_')) {
+        acc[key] = value
+        return acc
+      }
+
       const keyUnformatted = key.replaceAll('_', ' . ').replaceAll('-', ' ')
 
       const wordsToHardcode = {
