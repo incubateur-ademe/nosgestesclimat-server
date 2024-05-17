18cb7dfeccb9d8e655fd2297652e83bd4c0ad238
Florian <hi@florianpanchout.com>
2024-05-17 13:36:45 +0200

    HOTFIX : Stop computing results for polls with more than 100 simulations (#103)
    * feat: Setup basique
    
    * wip
    
    * feat: Ajoute script faker
    
    * feat: Ecrit soak et spike test
    
    * feat: Update endpoint
    
    * fix: URL
    
    * wip
    
    * wip
    
    * wip: ça fonctionne !!
    
    * feat: Prépare les tests pour la preprod
    
    * Update package.json
    
    * Update package.json
    
    * Update SimulationSchema.ts
    
    * feat: add lite publicodes logic
    
    * fix: Potentially fix memory issue
    
    * fixes
    
    * build
    
    * update
    
    * feat: Ajoute workflow github
    
    * ajoute workflows
    
    * corrige
    
    * Update create.ts
    
    * fix
    
    * chore: refacto ui metrics computing
    
    * feat: add new metrics
    
    * feat: Ajoute les routes
    
    * chore: fix types
    
    * feat: support "somme"
    
    * feat: Add limit
    
    * feat: Modifie le nombre d'utilisateurs + ajoute timeout (fonctionnel)
    
    * fix: formatage workflow
    
    * feat: Modifie le mode de sauvegarde des questions personnalisées
    
    * Update spike-load-tests.yml
    
    * Update spike-load-tests.yml
    
    * Update spike-load-tests.yml
    
    * feat: Ajoute la sauvegarde des réponses
    
    * fix: use dottednames with categories
    
    * pkg!: use ngc 2.4.0-rc.1
    
    * fix: Ajoute champ manquant
    
    * feat: update lock file
    
    * fix: allow pr url
    
    * feat: Ajoute le champ organisationType
    
    * pkg!: use ngc 2.4.0-rc.2 with ui infos
    
    * feat: upgrade to node 20
    
    * fix: unformat situation
    
    * Create recomputeResults.ts
    
    * Update computeResults.ts
    
    * fix: crée un seul engine
    
    * fix lint error
    
    * feat: Use get instead of post
    
    * fix: Corrections après relecture
    
    * feat: add comments
    
    * chore: plus besoin de la PR dans les sources
    
    * feat: Retire la mise à jour des computedResults à chaque fetch de gorup
    
    * chore: useless `let`
    
    * Update create.ts
    
    * build
    
    * fix: return
    
    * chore: update dist
    
    * feat: add new template and save unfinished simulations
    
    * feat: filter unfinished simulations for group and poll dashboards
    
    * wip: modifie recompute results en migrant les situations
    
    * feat: add index to simulation schema
    
    * feat: add new brevo list
    
    * chore: new lines
    
    * fix: correct url for spike test
    
    * Update unformatSituation.ts
    
    * feat: Implémente les helpers de formatage avec encodeURIComponent
    
    * fix: Corrige le script de migration + ajoute publicodes/tools
    
    * fix
    
    * fixes for the Panch
    
    * feat: Retire le formatage des clés de règles dans l'algo de stats orgas
    
    * fix: Retire code inutile
    
    * Update verifyUserParticipation.ts
    
    * fix: config
    
    * Update tsconfig.json
    
    * fix: Bévue
    
    * Update tsconfig.json
    
    * fix: Ts Config
    
    * chore: Mise à jour chemin
    
    * feat: unformat les simulations et compute les computedResults à la volée
    
    * feat: recompute every suspect result
    
    * fix: remove log and move engine init
    
    * chore: add doc
    
    * fix: handle _id and id correctly
    
    * feat: Do not compute results for polls with more than 100 simulations
    
    ---------
    
    Co-authored-by: Benjamin Arias <bjlaa@proton.me>
    Co-authored-by: Clément <clem.auger@hotmail.fr>
    Co-authored-by: Clément <55186402+Clemog@users.noreply.github.com>

--- src/routes/simulations/fetchSimulation.ts
+++ src/routes/simulations/fetchSimulation.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-
-import { Simulation, SimulationType } from '../../schemas/SimulationSchema'
-
+import { Simulation } from '../../schemas/SimulationSchema'
+import rules from '@incubateur-ademe/nosgestesclimat/public/co2-model.FR-lang.fr.json'
 import { setSuccessfulJSONResponse } from '../../utils/setSuccessfulResponse'
-import mongoose, { HydratedDocument } from 'mongoose'
-import { unformatSimulation } from '../../helpers/simulation/unformatSimulation'
+import mongoose from 'mongoose'
 import { handleComputeResultsIfNone } from '../../helpers/simulation/handleComputeResultsIfNone'
+import Engine from 'publicodes'
+import { NGCRules } from '@incubateur-ademe/nosgestesclimat'
 
 const router = express.Router()
 
@@ -31,7 +31,18 @@ router.route('/').post(async (req, res) => {
       return res.status(404).send('No matching simulation found.')
     }
 
-    const migratedSimulation = handleComputeResultsIfNone(simulationFound)
+    const engine = new Engine(rules as unknown as NGCRules, {
+      logger: {
+        log: console.log,
+        warn: () => null,
+        error: console.error,
+      },
+    })
+
+    const migratedSimulation = handleComputeResultsIfNone(
+      simulationFound,
+      engine
+    )
 
     setSuccessfulJSONResponse(res)
 
