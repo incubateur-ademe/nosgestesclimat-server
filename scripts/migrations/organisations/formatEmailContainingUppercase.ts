d01306fece76ca8e498f7059297bda05e62bb8ce
Benjamin Arias <bjlaa@proton.me>
2024-07-11 17:10:12 +0200

    feat: Ajoute le script de migration des adresses email

---
+++ scripts/migrations/organisations/formatEmailContainingUppercase.ts
@@ -0,0 +1,26 @@
+import { Organisation } from '../../../src/schemas/OrganisationSchema'
+import mongoose from 'mongoose'
+import { config } from '../../../src/config'
+
+async function formatEmailContainingUppercase() {
+  try {
+    mongoose.connect(config.mongo.url)
+
+    const organisations = await Organisation.find({
+      email: { $regex: /[A-Z]/ },
+    })
+
+    for (const organisation of organisations) {
+      const email = organisation.administrators[0].email.toLowerCase()
+
+      await Organisation.updateOne(
+        { _id: organisation._id },
+        { $set: { email } }
+      )
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+formatEmailContainingUppercase()
