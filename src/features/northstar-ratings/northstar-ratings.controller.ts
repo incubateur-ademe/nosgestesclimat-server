eba0918c201220557da8820835bb42c10b4698b3
Paul Souche <paul@bettercallpaul.fr>
2024-08-27 17:25:51 +0200

    :sparkles: northstar ratings creation

--- src/features/northstar-ratings/northstar-ratings.controller.ts
+++ src/features/northstar-ratings/northstar-ratings.controller.ts
@@ -1,5 +1,7 @@
 import express from 'express'
+import { StatusCodes } from 'http-status-codes'
 import { validateRequest } from 'zod-express-middleware'
+import logger from '../../logger'
 import { NorthstarRatingCreateValidator } from './northstar-ratings.validator'
 
 const router = express.Router()
@@ -7,6 +9,16 @@ const router = express.Router()
 /**
  * Creates a new northstar rating
  */
-router.route('/').post(validateRequest(NorthstarRatingCreateValidator))
+router
+  .route('/')
+  .post(validateRequest(NorthstarRatingCreateValidator), (req, res) => {
+    try {
+      return res.sendStatus(StatusCodes.CREATED).json({})
+    } catch (err) {
+      logger.error('NorthstarRating creation failed', err)
+
+      return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR).json({})
+    }
+  })
 
 export default router
