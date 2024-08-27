b0486a00c95ba71f055abb1f4d8da14d8f712a65
Paul Souche <paul@bettercallpaul.fr>
2024-08-27 17:19:46 +0200

    :test_tube: add a failing test

--- src/features/northstar-ratings/__tests__/create-northstar-ratings.spec.ts
+++ src/features/northstar-ratings/__tests__/create-northstar-ratings.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { StatusCodes } from 'http-status-codes'
 import supertest from 'supertest'
 import app from '../../../app'
@@ -11,6 +12,56 @@ describe('Given a NGC user', () => {
       test(`Then it should return a ${StatusCodes.BAD_REQUEST} error`, async () => {
         await agent.post(url).expect(StatusCodes.BAD_REQUEST)
       })
+    })
+
+    describe('And invalid simulationId', () => {
+      test(`Then it should return a ${StatusCodes.BAD_REQUEST} error`, async () => {
+        await agent
+          .post(url)
+          .send({
+            simulationId: faker.string.alpha(34),
+            value: 5,
+            type: 'learned',
+          })
+          .expect(StatusCodes.BAD_REQUEST)
+      })
+    })
+
+    describe('And invalid value', () => {
+      test(`Then it should return a ${StatusCodes.BAD_REQUEST} error`, async () => {
+        await agent
+          .post(url)
+          .send({
+            simulationId: faker.string.uuid(),
+            value: 42,
+            type: 'learned',
+          })
+          .expect(StatusCodes.BAD_REQUEST)
+      })
+    })
+
+    describe('And invalid type', () => {
+      test(`Then it should return a ${StatusCodes.BAD_REQUEST} error`, async () => {
+        await agent
+          .post(url)
+          .send({
+            simulationId: faker.string.uuid(),
+            value: 5,
+            type: 'my-invalid-type',
+          })
+          .expect(StatusCodes.BAD_REQUEST)
+      })
+    })
+
+    test(`It should return a ${StatusCodes.CREATED} response`, async () => {
+      await agent
+        .post(url)
+        .send({
+          simulationId: faker.string.uuid(),
+          value: 5,
+          type: 'learned',
+        })
+        .expect(StatusCodes.CREATED)
     })
   })
 })
