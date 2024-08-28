ef48e283b5646d118cfa4ed3bbe537f5a1a257f9
Paul Souche <paul@bettercallpaul.fr>
2024-08-28 10:35:25 +0200

    :poop: evaluate computedResults on fetch (NGC-1140) (#151)
    * :poop: evaluate computedResults on fetch (NGC-1140)
    
    * :poop: evaluate computedResults on fetch groups (NGC-1140)
    
    * :recycle: make engine really static

--- src/routes/groups/fetchGroup.ts
+++ src/routes/groups/fetchGroup.ts
@@ -1,8 +1,22 @@
+import migrationInstructionsJSON from '@incubateur-ademe/nosgestesclimat/public/migration.json'
+// @ts-expect-error Typing error in the library
+import { migrateSituation } from '@publicodes/tools/migration'
 import express from 'express'
+import { computeResults } from '../../helpers/simulation/computeResults'
+import type { GroupType, ParticipantType } from '../../schemas/GroupSchema'
 import { Group } from '../../schemas/GroupSchema'
+import { Simulation, type SimulationType } from '../../schemas/SimulationSchema'
 import { setSuccessfulJSONResponse } from '../../utils/setSuccessfulResponse'
 
 const router = express.Router()
+
+type PopulatedParticipantType = Omit<ParticipantType, 'simulation'> & {
+  simulation: SimulationType
+}
+
+type PopulatedGroupType = Omit<GroupType, 'participants'> & {
+  participants: PopulatedParticipantType[]
+}
 
 /**
  * Fetch a group
@@ -16,24 +30,49 @@ router.route('/').post(async (req, res) => {
   }
 
   try {
-    const group = await Group.findById(groupId).populate({
-      path: 'participants',
-      populate: {
-        path: 'simulation',
-        match: { progression: 1 },
-      },
-    })
+    const group = await Group.findById(groupId)
+      .populate<PopulatedGroupType>({
+        path: 'participants',
+        populate: {
+          path: 'simulation',
+          match: { progression: 1 },
+        },
+      })
+      .lean()
 
     // If no group is found, we return an error
     if (!group) {
       return res.status(404).send('Error. Group not found.')
     }
 
-    const groupObject = group.toObject()
+    for (const participant of group.participants) {
+      const { simulation } = participant
+
+      if (!simulation || !!simulation.computedResults) {
+        continue
+      }
+
+      const situationMigrated = migrateSituation(
+        simulation.situation,
+        migrationInstructionsJSON
+      )
+
+      const computedResults = computeResults(situationMigrated)
+
+      participant.simulation = (await Simulation.findByIdAndUpdate(
+        simulation._id,
+        {
+          computedResults: {
+            carbone: computedResults,
+          },
+        },
+        { new: true }
+      ).lean())!
+    }
 
     setSuccessfulJSONResponse(res)
 
-    res.json(groupObject)
+    res.json(group)
 
     console.log(`Group fetched: ${groupId}`)
   } catch (error) {
