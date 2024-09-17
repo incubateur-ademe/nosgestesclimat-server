847e6d6578c0a91b8ca8e7970854a087faae61ef
Paul Souche <paul@bettercallpaul.fr>
2024-09-17 22:29:08 +0200

    :recycle: update also in postgres

--- src/helpers/organisations/handleUpdateOrganisation.ts
+++ src/helpers/organisations/handleUpdateOrganisation.ts
@@ -1,4 +1,6 @@
 import type { Types } from 'mongoose'
+import { prisma } from '../../adapters/prisma/client'
+import logger from '../../logger'
 import { Organisation } from '../../schemas/OrganisationSchema'
 
 type Props = {
@@ -7,13 +9,51 @@ type Props = {
   updates: {
     email: string
     organisationName: string
-    uniqueSlug?: string
+    uniqueSlug: string
     administratorName: string
     position: string
     administratorTelephone: string
     hasOptedInForCommunications: boolean
     organisationType: string
     numberOfCollaborators: number
+  }
+}
+
+const getOrganisationType = (type?: string) => {
+  if (!type) {
+    return
+  }
+
+  switch (type) {
+    case 'Association':
+    case 'Asociación':
+      return 'association'
+    case 'Autre':
+    case 'Other':
+    case 'Otros':
+      return 'other'
+    case 'Company':
+    case 'Entreprise':
+    case 'Empresa':
+      return 'company'
+    case 'Coopérative':
+    case 'Cooperative':
+    case 'Cooperativa':
+      return 'cooperative'
+    case "Groupe d'amis":
+    case 'Group of friends':
+    case 'Grupo de amigos':
+      return 'groupOfFriends'
+    case 'Public ou collectivité territoriale':
+    case 'Public or local authority':
+    case 'Autoridad pública o local':
+      return 'publicOrRegionalAuthority'
+    case 'Université ou école':
+    case 'University or school':
+    case 'Universidad o escuela':
+      return 'universityOrSchool'
+    default:
+      throw new Error(`unknown organisation type ${type}`)
   }
 }
 
@@ -33,7 +73,7 @@ export async function handleUpdateOrganisation({
   },
 }: Props) {
   // Update organisation using findOneAndUpdate
-  return await Organisation.findOneAndUpdate(
+  const updatedOrganisation = await Organisation.findOneAndUpdate(
     {
       _id,
       'administrators.email': administratorEmail,
@@ -56,11 +96,69 @@ export async function handleUpdateOrganisation({
         ...(organisationType && { organisationType: organisationType }),
         ...(numberOfCollaborators && { numberOfCollaborators }),
         ...(email &&
-          email !== administratorEmail && { 'administrators.$.email': email }),
+          email !== administratorEmail && {
+            'administrators.$.email': email,
+          }),
       },
     },
     {
       new: true,
     }
   )
+
+  try {
+    await prisma.verifiedUser.update({
+      where: {
+        email: administratorEmail,
+      },
+      data: {
+        name: administratorName,
+        telephone: administratorTelephone,
+        optedInForCommunications: hasOptedInForCommunications,
+        position,
+        ...(email &&
+          email !== administratorEmail && {
+            email,
+          }),
+      },
+    })
+
+    await prisma.organisation.upsert({
+      where: {
+        id: _id.toString(),
+      },
+      create: {
+        id: _id.toString(),
+        name: organisationName,
+        slug: uniqueSlug,
+        type: getOrganisationType(organisationType),
+        numberOfCollaborators,
+        administrators: {
+          create: {
+            userEmail: administratorEmail,
+          },
+        },
+      },
+      update: {
+        name: organisationName,
+        slug: uniqueSlug,
+        type: getOrganisationType(organisationType),
+        numberOfCollaborators,
+        administrators: {
+          update: {
+            where: {
+              userEmail: administratorEmail,
+            },
+            data: {
+              userEmail: email,
+            },
+          },
+        },
+      },
+    })
+  } catch (error) {
+    logger.error('postgre Organisations replication failed', error)
+  }
+
+  return updatedOrganisation
 }
