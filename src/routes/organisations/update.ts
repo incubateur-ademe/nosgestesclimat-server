a5ccd00dac032dcf4d6c40771e54b393c75dcf6c
Paul Souche <paul@bettercallpaul.fr>
2024-10-21 20:23:10 +0200

    :recycle: auth middleware in unauthenticated mode (NGC-1310)

--- src/routes/organisations/update.ts
+++ src/routes/organisations/update.ts
@@ -22,7 +22,7 @@ const router = express.Router()
  * Fetching / updating by the owner
  * Needs to be authenticated and generates a new token at each request
  */
-router.use(authentificationMiddleware).post('/', async (req, res) => {
+router.use(authentificationMiddleware()).post('/', async (req, res) => {
   const email = formatEmail(req.body.email)
 
   if (!email) {
