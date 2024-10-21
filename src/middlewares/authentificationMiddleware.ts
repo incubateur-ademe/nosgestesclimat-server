4159c0d99defb6f08a4e8bd4f25127bc3cfc182a
Paul Souche <paul@bettercallpaul.fr>
2024-10-21 20:23:10 +0200

    :recycle: auth middleware in unauthenticated mode (NGC-1310)

--- src/middlewares/authentificationMiddleware.ts
+++ src/middlewares/authentificationMiddleware.ts
@@ -13,58 +13,62 @@ const isValidResult = (
 ): result is JwtPayload & { email: string; userId?: string } =>
   typeof result === 'object' && 'email' in result
 
-export const authentificationMiddleware: RequestHandler = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const cookiesHeader = req.headers.cookie
+export const authentificationMiddleware =
+  ({
+    passIfUnauthorized,
+  }: { passIfUnauthorized?: true } = {}): RequestHandler =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const cookiesHeader = req.headers.cookie
 
-  const token =
-    cookiesHeader &&
-    cookiesHeader
-      .split(';')
-      .map((c) => c.trim())
-      .find((cookie) => cookie.startsWith(COOKIE_NAME))
-      ?.replace(`${COOKIE_NAME}=`, '')
+    const token =
+      cookiesHeader &&
+      cookiesHeader
+        .split(';')
+        .map((c) => c.trim())
+        .find((cookie) => cookie.startsWith(COOKIE_NAME))
+        ?.replace(`${COOKIE_NAME}=`, '')
 
-  if (!token) {
-    return res.status(StatusCodes.UNAUTHORIZED).end()
-  }
-
-  jwt.verify(token, config.security.jwt.secret, async (err, result) => {
-    if (err || !isValidResult(result)) {
-      return res.status(StatusCodes.UNAUTHORIZED).end()
+    if (!token) {
+      return passIfUnauthorized
+        ? next()
+        : res.status(StatusCodes.UNAUTHORIZED).end()
     }
 
-    const { email } = result
-    let { userId } = result
-
-    if (!userId) {
-      try {
-        const { id } = await prisma.verifiedUser.findUniqueOrThrow({
-          where: {
-            email: result.email,
-          },
-          select: {
-            id: true,
-          },
-        })
-
-        userId = id
-      } catch (e) {
-        logger.warn(`Could not find verified user for ${result.email}`, e)
+    jwt.verify(token, config.security.jwt.secret, async (err, result) => {
+      if (err || !isValidResult(result)) {
+        return passIfUnauthorized
+          ? next()
+          : res.status(StatusCodes.UNAUTHORIZED).end()
       }
-    }
 
-    req.user = {
-      email,
-      userId: userId!,
-    }
+      const { email } = result
+      let { userId } = result
 
-    // Generate a new token
-    generateAndSetNewToken(res, req.user)
+      if (!userId) {
+        try {
+          const { id } = await prisma.verifiedUser.findUniqueOrThrow({
+            where: {
+              email: result.email,
+            },
+            select: {
+              id: true,
+            },
+          })
 
-    next()
-  })
-}
+          userId = id
+        } catch (e) {
+          logger.warn(`Could not find verified user for ${result.email}`, e)
+        }
+      }
+
+      req.user = {
+        email,
+        userId: userId!,
+      }
+
+      // Generate a new token
+      generateAndSetNewToken(res, req.user)
+
+      next()
+    })
+  }
