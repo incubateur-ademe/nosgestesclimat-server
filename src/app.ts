bc0d93ce5544522208707e43939e8637d7dd5141
Paul Souche <paul@bettercallpaul.fr>
2024-10-30 14:54:08 +0100

    :fire: remove old API

--- src/app.ts
+++ src/app.ts
@@ -12,57 +12,22 @@ import statsRoute from './routes/stats/statsRoute'
 import authenticationController from './features/authentication/authentication.controller'
 import verificationCodeController from './features/authentication/verification-codes.controller'
 
-// Groups routes
 import groupsController from './features/groups/groups.controller'
-import createGroupRoute from './routes/groups/createGroup'
-import deleteGroupRoute from './routes/groups/deleteGroup'
-import fetchGroupRoute from './routes/groups/fetchGroup'
-import updateGroupRoute from './routes/groups/updateGroup'
-
-// Group participants routes
-import fetchGroupsRoute from './routes/groups/fetchGroups'
-import removeParticipantRoute from './routes/groups/removeParticipant'
 
 // Integrations routes
 import integrationsController from './features/integrations/integrations.controller'
 
 // Organisation routes
 import organisationController from './features/organisations/organisations.controller'
-import createOrganisationRoute from './routes/organisations/create'
-import fetchOrganisationRoute from './routes/organisations/fetchOrganisation'
-import getOrgaPollSlugsRoute from './routes/organisations/getOrgaPollSlugs'
-import loginOrganisationRoute from './routes/organisations/login'
-import logOutRoute from './routes/organisations/logout'
-import sendVerificationCodeRoute from './routes/organisations/sendVerificationCode'
-import sendVerificationCodeWhenModifyingEmail from './routes/organisations/sendVerificationCodeWhenModifyingEmail'
-import updateRoute from './routes/organisations/update'
-import updateAdministratorEmail from './routes/organisations/updateAdministratorEmail'
-import validateVerificationCodeRoute from './routes/organisations/validateVerificationCode'
-
-// Polls routes
-import checkCustomQuestionsEnabledRoute from './routes/polls/checkCustomQuestionsEnabled'
-import createPollRoute from './routes/polls/create'
-import deletePollRoute from './routes/polls/deletePoll'
-import fetchPoll from './routes/polls/fetchPoll'
-import fetchPollProcessedData from './routes/polls/fetchPollProcessedData'
-import fetchPollPublicInfoRoute from './routes/polls/fetchPollPublicInfo'
-import fetchPollsRoute from './routes/polls/fetchPolls'
-import updateCustomQuestionsRoute from './routes/polls/updateCustomQuestions'
-import updatePollRoute from './routes/polls/updatePoll'
-import verifyUserParticipationRoute from './routes/polls/verifyUserParticipation'
 
 // Simulation routes
 import simulationController from './features/simulations/simulations.controller'
-import createSimulationRoute from './routes/simulations/create'
-import fetchSimulationRoute from './routes/simulations/fetchSimulation'
 
 // Quiz routes
 import quizzAnswersController from './features/quizz-answers/quizz-answers.controller'
-import createQuizAnswerRoute from './routes/quiz/create'
 
 // Northstar routes
 import northstarRatingsController from './features/northstar-ratings/northstar-ratings.controller'
-import createNorthstarRatingRoute from './routes/northstar/create'
 
 // Settings route
 import getNewsletterSubscriptions from './routes/settings/getNewsletterSubscriptions'
@@ -104,73 +69,26 @@ app.use('/ratings', ratingsRoute)
 app.use('/authentication', authenticationController)
 app.use('/verification-codes', verificationCodeController)
 
-// Simulations route
-app.use('/simulations', simulationController)
-app.use('/simulations/create', createSimulationRoute)
-app.use('/simulations/fetch-simulation', fetchSimulationRoute)
-
 // Group routes
-app.use('/group/fetch', fetchGroupRoute)
-app.use('/group/create', createGroupRoute)
-app.use('/group/update', updateGroupRoute)
-app.use('/group/delete', deleteGroupRoute)
 app.use('/groups', groupsController)
 
-// Group participants routes
-app.use('/group/fetch-groups', fetchGroupsRoute)
-app.use('/group/remove-participant', removeParticipantRoute)
-
-//Integrations routes
+// Integration routes
 app.use('/integrations', integrationsController)
+
+// Northstar ratings routes
+app.use('/northstar-ratings', northstarRatingsController)
 
 // Organisation routes
 app.use('/organisations', organisationController)
-app.use('/organisations/create', createOrganisationRoute)
-app.use('/organisations/login', loginOrganisationRoute)
-app.use('/organisations/fetch-organisation', fetchOrganisationRoute)
-app.use('/organisations/update', updateRoute)
-app.use(
-  '/organisations/validate-verification-code',
-  validateVerificationCodeRoute
-)
-app.use('/organisations/send-verification-code', sendVerificationCodeRoute)
-app.use('/organisations/logout', logOutRoute)
-app.use(
-  '/organisations/verify-user-participation',
-  verifyUserParticipationRoute
-)
-app.use('/organisations/get-orga-poll-slugs', getOrgaPollSlugsRoute)
-app.use('/organisations/verify-and-update', updateAdministratorEmail)
-app.use(
-  '/organisations/send-verification-code-when-modifying-email',
-  sendVerificationCodeWhenModifyingEmail
-)
 
-// Polls routes
-app.use('/polls/create', createPollRoute)
-app.use('/polls/update', updatePollRoute)
-app.use('/polls/delete', deletePollRoute)
-app.use('/polls/fetch-poll', fetchPoll)
-app.use('/polls/fetch-public-poll', fetchPollPublicInfoRoute)
-
-app.use('/polls/fetch-polls', fetchPollsRoute)
-app.use('/polls/fetch-poll-processed-data', fetchPollProcessedData)
-app.use(
-  '/polls/check-custom-questions-enabled',
-  checkCustomQuestionsEnabledRoute
-)
-app.use('/polls/update-custom-questions', updateCustomQuestionsRoute)
-
-// quizz-answers routes
+// Quizz Answers routes
 app.use('/quizz-answers', quizzAnswersController)
-app.use('/quiz/answers/create', createQuizAnswerRoute)
-
-// northstar-ratings routes
-app.use('/northstar-ratings', northstarRatingsController)
-app.use('/northstar/ratings/create', createNorthstarRatingRoute)
 
 // Settings route
 app.use('/update-settings', updateSettingsRoute)
 app.use('/get-newsletter-subscriptions', getNewsletterSubscriptions)
+
+// Simulations route
+app.use('/simulations', simulationController)
 
 export default app
--- src/utils/getSlug.ts
+++
@@ -1,7 +0,0 @@
-import slugify from 'slugify'
-
-export function getSlug(text: string) {
-  return slugify(text.toLowerCase(), {
-    strict: true,
-  })
-}
