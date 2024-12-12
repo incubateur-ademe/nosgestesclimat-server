8745fb26a2c0ef1739ec7b9ee488b7eb8cc2c979
Clément <clem.auger@hotmail.fr>
2024-12-12 11:56:17 +0100

    feat: create view for `bilan`

---
+++ prisma/migrations/20241211150143_computed_results_view/migration.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW ngc."ComputedResultsView" AS
+SELECT id,
+    date,
+    "progression",
+    "computedResults",
+    "createdAt",
+    "updatedAt",
+    "computedResults"->'eau'->'bilan' as bilanEau,
+    "computedResults"->'carbone'->'bilan' as bilanCarbone
+FROM ngc."Simulation"
