8dc8995f90e7ced6903ce1a245e0bb523bff778b
Paul Souche <paul@bettercallpaul.fr>
2024-12-23 11:14:44 +0100

    :sparkles: newsletter routes refactored (#206)
    * :sparkles: get newsletter subscribers
    
    * :sparkles: get user contact
    
    * :sparkles: update user contact

--- src/features/users/users.validator.ts
+++ src/features/users/users.validator.ts
@@ -1,4 +1,6 @@
 import z from 'zod'
+import { ListIds } from '../../adapters/brevo/constant'
+import { EMAIL_REGEX } from '../../core/typeguards/isValidEmail'
 
 export const UserParams = z
   .object({
@@ -7,3 +9,29 @@ export const UserParams = z
   .strict()
 
 export type UserParams = z.infer<typeof UserParams>
+
+export const FetchUserBrevoContactValidator = {
+  body: z.object({}).strict().optional(),
+  params: UserParams,
+  query: z.object({}).strict().optional(),
+}
+
+const UserBrevoContactUpdateDto = z
+  .object({
+    email: z
+      .string()
+      .regex(EMAIL_REGEX)
+      .transform((email) => email.toLocaleLowerCase()),
+    listIds: z.array(z.nativeEnum(ListIds)),
+  })
+  .strict()
+
+export type UserBrevoContactUpdateDto = z.infer<
+  typeof UserBrevoContactUpdateDto
+>
+
+export const UpdateUserBrevoContactValidator = {
+  body: UserBrevoContactUpdateDto,
+  params: UserParams,
+  query: z.object({}).strict().optional(),
+}
