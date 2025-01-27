fdd56888bbc631b470d4d1cd67fe753c407a6eeb
Paul Souche <paul@bettercallpaul.fr>
2025-01-27 13:01:12 +0100

    🐛 fix sentry issue (#228)
    * :arrow_up: publicode
    
    * :bug: fix sentry issue

--- src/constants/publicode.ts
+++ src/constants/publicode.ts
@@ -2,6 +2,10 @@ import rules from '@incubateur-ademe/nosgestesclimat/public/co2-model.FR-lang.fr
 import Engine from 'publicodes'
 
 export const engine = new Engine(rules, {
+  strict: {
+    situation: false,
+    noOrphanRule: false,
+  },
   logger: {
     log: () => null,
     warn: () => null,
