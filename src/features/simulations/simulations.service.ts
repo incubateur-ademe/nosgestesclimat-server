084e23d154ce4d2b4c86e03a7c0533c56c1b7637
Paul Souche <paul@bettercallpaul.fr>
2025-02-10 18:05:48 +0100

    :recycle: put ugly funfacts code back in place (#242)
    * :recycle: put ugly funfacts code back in place
    
    * :test_tube: compare engine and algorithm
    
    * :green_heart: fix tests except averageOfElectricityConsumption
    
    * :recycle: remove ugly compute on average funfacts
    
    * :recycle: remove global engine (could not change the rules anyway)
    
    * :arrow_up: nosgestesclimat model

--- src/features/simulations/simulations.service.ts
+++ src/features/simulations/simulations.service.ts
@@ -7,11 +7,9 @@ import modelRules from '@incubateur-ademe/nosgestesclimat/public/co2-model.FR-la
 import modelFunFacts from '@incubateur-ademe/nosgestesclimat/public/funFactsRules.json'
 import type { JsonValue } from '@prisma/client/runtime/library'
 import type { Request } from 'express'
-import { engine } from '../../constants/publicode'
 import { EntityNotFoundException } from '../../core/errors/EntityNotFoundException'
 import { EventBus } from '../../core/event-bus/event-bus'
 import { isPrismaErrorNotFound } from '../../core/typeguards/isPrismaError'
-import logger from '../../logger'
 import { PollUpdatedEvent } from '../organisations/events/PollUpdated.event'
 import type { PublicPollParams } from '../organisations/organisations.validator'
 import type { UserParams } from '../users/users.validator'
@@ -24,16 +22,15 @@ import {
   fetchUserSimulations,
 } from './simulations.repository'
 import type {
+  SimulationCreateDto,
   SimulationCreateNewsletterList,
   UserSimulationParams,
 } from './simulations.validator'
-import {
-  ComputedResultSchema,
-  SituationSchema,
-  type SimulationCreateDto,
-} from './simulations.validator'
+import { ComputedResultSchema, SituationSchema } from './simulations.validator'
+import type { Rules } from './situation/situation.service'
+import { getSituationDottedNameValue } from './situation/situation.service'
 
-const rules = modelRules as Record<DottedName, NGCRule | string | null>
+const frRules = modelRules as Record<DottedName, NGCRule | string | null>
 const funFactsRules = modelFunFacts as { [k in keyof FunFacts]: DottedName }
 
 const simulationToDto = (
@@ -205,75 +202,29 @@ type FunFactsSimulations = Array<{
   situation: SituationSchema
 }>
 
-const getSituationDottedNameValue = (
-  situation: SituationSchema,
+const getFunFactValue = ({
+  dottedName,
+  simulations,
+  rules,
+}: {
   dottedName: DottedName
-): number => {
-  try {
-    engine.setSituation(situation)
-
-    const value = engine.evaluate(dottedName).nodeValue
-
-    if (typeof value === 'number' && !!value) {
-      return value
-    }
-
-    if (value === true) {
-      return 1
-    }
-
-    return 0
-  } catch (error) {
-    logger.error(`Cannot evaluate dottedName ${dottedName}`, {
-      situation,
-      error,
-    })
-
-    return 0
-  }
-}
-
-const getFunFactValue = (
-  dottedName: DottedName,
   simulations: FunFactsSimulations
-): number =>
+  rules: Rules
+}): number =>
   simulations.reduce(
     (acc, { situation }) =>
-      acc + getSituationDottedNameValue(situation, dottedName),
+      acc + getSituationDottedNameValue({ dottedName, situation, rules }),
     0
   )
-
-const specialAverageKeys = new Set<string>(<(keyof FunFacts)[]>[
-  'averageOfCarKilometers',
-  'averageOfTravelers',
-  'averageOfElectricityConsumption',
-])
 
 const getSimulationsFunFacts = (simulations: FunFactsSimulations) => {
   return Object.fromEntries(
     Object.entries(funFactsRules).map(([key, dottedName]) => {
-      if (dottedName in rules) {
-        let value = getFunFactValue(dottedName, simulations)
+      if (dottedName in frRules) {
+        let value = getFunFactValue({ dottedName, simulations, rules: frRules })
 
-        const rule = rules[dottedName]
-        if (
-          !!rule &&
-          typeof rule === 'object' &&
-          specialAverageKeys.has(key) &&
-          typeof rule.formule === 'object' &&
-          Array.isArray(rule.formule?.moyenne)
-        ) {
-          const [moyenneDottedName] = rule.formule.moyenne
-          if (typeof moyenneDottedName === 'string') {
-            const totalAnswers = simulations.reduce(
-              (acc, { situation }) =>
-                acc + (moyenneDottedName in situation ? 1 : 0),
-              0
-            )
-            if (totalAnswers > 0) {
-              value = value / totalAnswers
-            }
-          }
+        if (key.startsWith('average')) {
+          value = value / simulations.length
         }
 
         if (key.startsWith('percentage')) {
---
+++ src/features/simulations/situation/situation.service.ts
@@ -0,0 +1,230 @@
+import type { DottedName, NGCRule } from '@incubateur-ademe/nosgestesclimat'
+import logger from '../../../logger'
+import type { SituationSchema } from '../simulations.validator'
+
+export type Rules = Record<DottedName, NGCRule | string | null>
+
+const isDottedName = (dottedName: unknown): dottedName is DottedName =>
+  typeof dottedName === 'string'
+
+const safeEvaluateSituationDottedNameDefaultValue = ({
+  dottedName,
+  situation,
+  rules,
+}: {
+  situation: SituationSchema
+  dottedName: DottedName
+  rules: Rules
+}): number => {
+  const rule = rules[dottedName]
+
+  if (!rule || typeof rule !== 'object') {
+    return 0
+  }
+
+  if (
+    'applicable si' in rule &&
+    !checkIfConditionIsTrue({
+      conditionDottedName: [
+        ...dottedName.split(' . ').slice(0, -1),
+        rule['applicable si'],
+      ].join(' . '),
+      situation,
+      rules,
+    })
+  ) {
+    return 0
+  }
+
+  if ('par défaut' in rule && typeof rule['par défaut'] === 'number') {
+    return rule['par défaut']
+  }
+
+  return 0
+}
+
+const safeEvaluateSituationDottedName = ({
+  dottedName,
+  situation,
+  rules,
+}: {
+  situation: SituationSchema
+  dottedName: unknown
+  rules: Rules
+}): number => {
+  if (!isDottedName(dottedName)) {
+    return 0
+  }
+
+  const rawValue = situation[dottedName]
+
+  return typeof rawValue === 'object'
+    ? safeEvaluateSituationDottedNameDefaultValue({
+        dottedName,
+        situation,
+        rules,
+      })
+    : Number.isNaN(+rawValue)
+      ? safeEvaluateSituationDottedNameDefaultValue({
+          dottedName,
+          situation,
+          rules,
+        })
+      : +rawValue
+}
+
+type Operator = '<' | '>' | '='
+
+const OPERATORS = new Set(['>', '<', '='])
+
+const isOperator = (raw: string): raw is Operator => OPERATORS.has(raw)
+
+const evaluateConditions = (
+  operator: Operator,
+  {
+    left,
+    right,
+  }: { left: number; right: number } | { left: string; right: string }
+) => {
+  switch (operator) {
+    case '<':
+      return left < right
+    case '>':
+      return left > right
+    case '=':
+      return left === right
+  }
+}
+
+const checkIfConditionIsTrue = ({
+  conditionDottedName,
+  situation,
+  rules,
+}: {
+  situation: SituationSchema
+  conditionDottedName: unknown
+  rules: Rules
+}): boolean => {
+  if (!isDottedName(conditionDottedName)) {
+    return false
+  }
+
+  const [dottedName, operator, value] = conditionDottedName
+    .split(/(\s*[=<>]\s*)/)
+    .map((s) => s.trim())
+
+  if (!dottedName) {
+    return false
+  }
+
+  if (!operator && !value) {
+    return situation[dottedName] === 'oui'
+  }
+
+  const left =
+    typeof situation[dottedName] === 'string'
+      ? situation[dottedName]
+      : safeEvaluateSituationDottedName({
+          dottedName,
+          situation,
+          rules,
+        })
+  if (isOperator(operator) && value) {
+    if (Number.isNaN(+value)) {
+      return typeof left === 'string'
+        ? evaluateConditions(operator, { left, right: value })
+        : false
+    }
+    return typeof left === 'number'
+      ? evaluateConditions(operator, { left, right: +value })
+      : false
+  }
+
+  return false
+}
+
+export const getSituationDottedNameValue = ({
+  dottedName,
+  situation,
+  rules,
+}: {
+  situation: SituationSchema
+  dottedName: DottedName
+  rules: Rules
+}): number => {
+  try {
+    const rule = rules[dottedName]
+
+    if (
+      !rule ||
+      typeof rule === 'string' ||
+      !rule.formule ||
+      typeof rule.formule !== 'object'
+    ) {
+      return 0
+    }
+
+    const { formule } = rule
+
+    if ('moyenne' in formule && Array.isArray(formule.moyenne)) {
+      const [moyenneDottedName] = formule.moyenne
+      return safeEvaluateSituationDottedName({
+        situation,
+        dottedName: moyenneDottedName,
+        rules,
+      })
+    }
+
+    if ('somme' in formule && Array.isArray(formule.somme)) {
+      return formule.somme.reduce(
+        (acc, sommeDottedName) =>
+          acc +
+          safeEvaluateSituationDottedName({
+            situation,
+            dottedName: sommeDottedName,
+            rules,
+          }),
+        0
+      )
+    }
+
+    if (
+      'une de ces conditions' in formule &&
+      Array.isArray(formule['une de ces conditions'])
+    ) {
+      return formule['une de ces conditions'].some((conditionDottedName) =>
+        checkIfConditionIsTrue({
+          conditionDottedName,
+          situation,
+          rules,
+        })
+      )
+        ? 1
+        : 0
+    }
+
+    if (
+      'toutes ces conditions' in formule &&
+      Array.isArray(formule['toutes ces conditions'])
+    ) {
+      return formule['toutes ces conditions'].every((conditionDottedName) =>
+        checkIfConditionIsTrue({
+          conditionDottedName,
+          situation,
+          rules,
+        })
+      )
+        ? 1
+        : 0
+    }
+
+    return 0
+  } catch (error) {
+    logger.error(`Cannot evaluate dottedName ${dottedName}`, {
+      situation,
+      error,
+    })
+
+    return 0
+  }
+}
