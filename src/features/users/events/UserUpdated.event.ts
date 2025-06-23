75e4b246914a798688113644ecf7f165077ad057
Paul Souche <paul@bettercallpaul.fr>
2025-06-23 15:18:11 +0200

    :recycle: enum are bad (NGC-2045)

--- src/features/users/events/UserUpdated.event.ts
+++ src/features/users/events/UserUpdated.event.ts
@@ -1,13 +1,12 @@
 import type { User } from '@prisma/client'
 import type { BrevoContact } from '../../../adapters/brevo/client'
-import type { ListIds } from '../../../adapters/brevo/constant'
 import { EventBusEvent } from '../../../core/event-bus/event'
 
 export class UserUpdatedEvent extends EventBusEvent<{
   user: Pick<User, 'id' | 'name' | 'email'>
   newsletters: {
-    newslettersToUnsubscribe: Set<ListIds>
-    finalNewsletters: Set<ListIds>
+    newslettersToUnsubscribe: Set<number>
+    finalNewsletters: Set<number>
   }
   previousContact?: BrevoContact
   nextEmail?: string | null
