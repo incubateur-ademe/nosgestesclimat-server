c0c8f77826a034aea6ff175f573e4d35169f54aa
Paul Souche <paul@bettercallpaul.fr>
2025-09-08 13:59:37 +0200

    :adhesive_bandage: sanitize organisations administrators names (NGC-2309) (#343)

--- src/features/organisations/events/OrganisationCreated.event.ts
+++ src/features/organisations/events/OrganisationCreated.event.ts
@@ -1,12 +1,19 @@
 import type { Organisation, VerifiedUser } from '@prisma/client'
 import { EventBusEvent } from '../../../core/event-bus/event.js'
 import type { Locales } from '../../../core/i18n/constant.js'
+import { sanitizeOrganisationAdministratorName } from './event.mapper.js'
 
-export class OrganisationCreatedEvent extends EventBusEvent<{
+export type OrganisationCreatedEventAttributes = {
   organisation: Organisation & { administrators: Array<{ user: VerifiedUser }> }
   administrator: VerifiedUser
   locale: Locales
   origin: string
-}> {
+}
+
+export class OrganisationCreatedEvent extends EventBusEvent<OrganisationCreatedEventAttributes> {
   name = 'OrganisationCreatedEvent'
+
+  constructor(attributes: OrganisationCreatedEventAttributes) {
+    super(sanitizeOrganisationAdministratorName(attributes))
+  }
 }
--- src/features/organisations/events/PollCreated.event.ts
+++ src/features/organisations/events/PollCreated.event.ts
@@ -1,9 +1,16 @@
 import type { Organisation, Poll, VerifiedUser } from '@prisma/client'
 import { EventBusEvent } from '../../../core/event-bus/event.js'
+import { sanitizeOrganisationAdministratorName } from './event.mapper.js'
 
-export class PollCreatedEvent extends EventBusEvent<{
+export type PollCreatedEventAttributes = {
   organisation: Organisation & { administrators: Array<{ user: VerifiedUser }> }
   poll: Poll
-}> {
+}
+
+export class PollCreatedEvent extends EventBusEvent<PollCreatedEventAttributes> {
   name = 'PollCreatedEvent'
+
+  constructor(attributes: PollCreatedEventAttributes) {
+    super(sanitizeOrganisationAdministratorName(attributes))
+  }
 }
